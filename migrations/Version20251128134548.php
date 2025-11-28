8e8962e9de203bb83315c4b8d8f77f6a6fa916d2
msallembien <sobek@mervyn.>
2025-11-28 14:54:19 +0100

    update easyadmin

---
+++ migrations/Version20251128134548.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20251128134548 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE beacon (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, longitude DOUBLE PRECISION NOT NULL, latitude DOUBLE PRECISION NOT NULL, type VARCHAR(255) NOT NULL, is_placed TINYINT(1) NOT NULL, placed_at DATETIME DEFAULT NULL, created_at DATETIME DEFAULT NULL, qr VARCHAR(255) NOT NULL, beacon LONGTEXT DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE beacon_course (beacon_id INT NOT NULL, course_id INT NOT NULL, INDEX IDX_4EDA6FBCF6AD5578 (beacon_id), INDEX IDX_4EDA6FBC591CC992 (course_id), PRIMARY KEY(beacon_id, course_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE boundaries_course (id INT AUTO_INCREMENT NOT NULL, course_id INT DEFAULT NULL, longitude DOUBLE PRECISION NOT NULL, latitude DOUBLE PRECISION NOT NULL, INDEX IDX_1BEB33BC591CC992 (course_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE course (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, name VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL, status VARCHAR(255) NOT NULL, create_at DATETIME NOT NULL, placement_completed_at DATETIME NOT NULL, update_at DATETIME NOT NULL, INDEX IDX_169E6FB9A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE establishment (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE log_session (id INT AUTO_INCREMENT NOT NULL, runner_id INT DEFAULT NULL, type VARCHAR(255) NOT NULL, time DATETIME NOT NULL, latitude DOUBLE PRECISION DEFAULT NULL, longitude DOUBLE PRECISION DEFAULT NULL, additional_data LONGTEXT DEFAULT NULL, INDEX IDX_E889ED433C7FB593 (runner_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE runner (id INT AUTO_INCREMENT NOT NULL, session_id INT DEFAULT NULL, name VARCHAR(255) NOT NULL, departure DATETIME NOT NULL, arrival DATETIME NOT NULL, INDEX IDX_F92B8B3E613FECDF (session_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE session (id INT AUTO_INCREMENT NOT NULL, course_id INT DEFAULT NULL, session_name VARCHAR(255) NOT NULL, nb_runner INT NOT NULL, INDEX IDX_D044D5D4591CC992 (course_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE beacon_course ADD CONSTRAINT FK_4EDA6FBCF6AD5578 FOREIGN KEY (beacon_id) REFERENCES beacon (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE beacon_course ADD CONSTRAINT FK_4EDA6FBC591CC992 FOREIGN KEY (course_id) REFERENCES course (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE boundaries_course ADD CONSTRAINT FK_1BEB33BC591CC992 FOREIGN KEY (course_id) REFERENCES course (id)');
+        $this->addSql('ALTER TABLE course ADD CONSTRAINT FK_169E6FB9A76ED395 FOREIGN KEY (user_id) REFERENCES `user` (id)');
+        $this->addSql('ALTER TABLE log_session ADD CONSTRAINT FK_E889ED433C7FB593 FOREIGN KEY (runner_id) REFERENCES runner (id)');
+        $this->addSql('ALTER TABLE runner ADD CONSTRAINT FK_F92B8B3E613FECDF FOREIGN KEY (session_id) REFERENCES session (id)');
+        $this->addSql('ALTER TABLE session ADD CONSTRAINT FK_D044D5D4591CC992 FOREIGN KEY (course_id) REFERENCES course (id)');
+        $this->addSql('ALTER TABLE user ADD establishment_id INT DEFAULT NULL, DROP lastname, DROP first_name');
+        $this->addSql('ALTER TABLE user ADD CONSTRAINT FK_8D93D6498565851 FOREIGN KEY (establishment_id) REFERENCES establishment (id)');
+        $this->addSql('CREATE INDEX IDX_8D93D6498565851 ON user (establishment_id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE `user` DROP FOREIGN KEY FK_8D93D6498565851');
+        $this->addSql('ALTER TABLE beacon_course DROP FOREIGN KEY FK_4EDA6FBCF6AD5578');
+        $this->addSql('ALTER TABLE beacon_course DROP FOREIGN KEY FK_4EDA6FBC591CC992');
+        $this->addSql('ALTER TABLE boundaries_course DROP FOREIGN KEY FK_1BEB33BC591CC992');
+        $this->addSql('ALTER TABLE course DROP FOREIGN KEY FK_169E6FB9A76ED395');
+        $this->addSql('ALTER TABLE log_session DROP FOREIGN KEY FK_E889ED433C7FB593');
+        $this->addSql('ALTER TABLE runner DROP FOREIGN KEY FK_F92B8B3E613FECDF');
+        $this->addSql('ALTER TABLE session DROP FOREIGN KEY FK_D044D5D4591CC992');
+        $this->addSql('DROP TABLE beacon');
+        $this->addSql('DROP TABLE beacon_course');
+        $this->addSql('DROP TABLE boundaries_course');
+        $this->addSql('DROP TABLE course');
+        $this->addSql('DROP TABLE establishment');
+        $this->addSql('DROP TABLE log_session');
+        $this->addSql('DROP TABLE runner');
+        $this->addSql('DROP TABLE session');
+        $this->addSql('DROP INDEX IDX_8D93D6498565851 ON `user`');
+        $this->addSql('ALTER TABLE `user` ADD lastname VARCHAR(255) NOT NULL, ADD first_name VARCHAR(255) NOT NULL, DROP establishment_id');
+    }
+}
